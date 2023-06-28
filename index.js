2e501626576afbbcad73966f75972ee2a887067e
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-06-28 17:22:36 -0300

    show questions logic

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 
 const questions = [
   "O que aprendi hoje?",
-  "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+  "O que me deixou aborrecido?",
   "O que eu poderia fazer para melhorar?",
   "O que me deixou feliz hoje?",
   "Quantas pessoas ajudei hoje?",
 ];
 
 const ask = (index = 0) => {
-  process.stdout.write(questions[index] + "\n\n\n");
+  process.stdout.write("\n\n" + questions[index] + " > ");
 };
 
-ask(2);
+ask();
 
+const answers = [];
 process.stdin.on("data", data => {
-  process.stdout.write(data.toString().trim() + "\n");
-  process.exit();
-})
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length)
+  } else {
+    console.log(answers);
+    process.exit();
+  }
+});
